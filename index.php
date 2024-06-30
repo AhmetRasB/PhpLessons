0475e36886f760c28fa9f1d044d01fa310654e44
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-06-30 22:24:56 +0300

    yeni acıklamalar eklendi

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Square Calculator</title>
+</head>
+<body>
+
+
+
+<form action="index.php" method="post">
+    <input type="text" id="number" name="value">
+    <input type="submit" value="Calculate">
+    <?php
+    if(is_numeric($_POST["value"])){
+        $Deger = $_POST['value'];
+        function square() {
+            global $Deger;
+            return $Deger * $Deger;
+        }
+        echo "<p>" . "The square of $Deger is " . square();
+    }else
+    {
+        echo "<br>". "Please enter a number";
+    }
+    ?>
+</form>
+</body>
+</html>
