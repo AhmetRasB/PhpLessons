323754142d03855048bf6373f472ba70f3d37d2d
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-07-02 14:11:58 +0300

    merimanın adını ekledim

--- index6.php
+++ index6.php
@@ -1,18 +1,4 @@
 <?php
-$var = "8";
-echo gettype($var);
-settype($var, "integer");
-echo "<br>";
-echo $var;
-echo gettype($var);
-$variable = 123;
-
-echo gettype($variable); // Çıktı: integer
-
-if (is_int($variable)) {
-    echo "Değişken bir tamsayıdır.";
-} else {
-    echo "Değişken bir tamsayı değildir.";
-}
+echo "Benim adım meriman";
 
 ?>
