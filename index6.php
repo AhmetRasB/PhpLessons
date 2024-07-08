1014cef54e0468402171f17ebb583dc39676b34d
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-07-08 21:52:05 +0300

    Günün ilk mesajı

--- index6.php
+++ index6.php
@@ -4,24 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Çerçeveli Form</title>
-    <style>
-        .container {
-            border: 2px solid black; /* Çerçevenin kalınlığı ve rengi */
-            padding: 20px;          /* Çerçeve içindeki boşluk */
-            width: 300px;           /* Konteynerin genişliği */
-            margin: 0 auto;         /* Ortalamak için */
-            text-align: center;     /* Metin hizalaması */
-        }
-    </style>
+    <?php
+        echo(mt_rand(10,20));
+    ?>
 </head>
 <body>
-<div class="container">
-    <form action="index6.php" method="post">
-        <input type="hidden" name="form" value="form1">
-        <input type="text" name="test">
-        <input type="submit" value="Gönder">
 
-    </form>
+
+
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['form'] == 'form1') {
@@ -37,30 +27,11 @@
 
     }
     ?>
-</div>
 
-<div class="container">
-    <form action="index6.php" method="post">
-        <input type="hidden" name="form" value="form2">
-        <input type="text" name="testt">
-        <input type="submit" value="Gönder2">
-
-    </form>
 
     <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['form'] == 'form2') {
-        $gelen2 = $_POST['testt'];
 
-        // Değerin float olup olmadığını kontrol et
-        if (is_numeric($gelen2)) {
-            $gelenFloat2 = (float)$gelen2;
-            echo ceil($gelenFloat2);
-        } else {
-            echo "Lütfen geçerli bir sayı giriniz.";
-        }
-
-    }
     ?>
-</div>
+
 </body>
 </html>
